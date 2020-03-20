372cad1933e19231ddb2a481fc9a8cdd515c7c24
bryanbilek <you@example.com>
2020-03-20 11:58:00 -0500

    form for post request made, working

--- smurfs/src/components/PostSmurfs.js
+++ smurfs/src/components/PostSmurfs.js
@@ -3,55 +3,55 @@ import { useForm } from "react-hook-form"
 import axios from "axios";
 
 const PostSmurfs = () => {
-    return(<></>)
+    
+    const [smurf, setSmurf] = useState({
+        name: '',
+        age: '',
+        height: ''
+    });
 
-//     const [smurf, setSmurf] = useState({
-//         name: '',
-//         age: '',
-//         height: ''
-//     });
+    const { register, handleSubmit } = useForm();
 
-//     const { register, handleSubmit } = useForm();
+    const onSubmit = (data, e) => {
+        e.preventDefault();
+        axios
+        .post('http://localhost:3333/smurfs', data)
+        .then(res => {
+            console.log('response', res)
+        })
+        .catch(err => console.log('post req error', err))
+    }
 
-//     const onSubmit = (data, e) => {
-//         e.preventDefault();
-//         axios
-//         .post()
-//         .then(res => {
-//             console.log('response', res)
-//         })
-//         .catch(err => console.log('post req error', err))
-//     }
+    const handleChanges = e => {
+        setSmurf({...smurf, [e.target.name]: e.target.value})
+    }
 
-//     const handleChanges = e => {
-//         setSmurf(...smurf, [e.target.name]: e.target.value)
-//     }
+    return (<div className="smurf-form">
+        <h1>Add Smurfs To The Village!</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
 
-//     return (<div className="smurf-form">
-//         <h1>Add Smurfs To The Village!</h1>
-//         <form onSubmit={handlesubmit(onSubmit)}>
+            <input className="form-input"
+                type="text" placeholder="name"
+                name="name"
+                onChange={handleChanges}
+                ref={register({ required: true })} />
 
-//             <input className="form-input"
-//                 type="text" placeholder="name"
-//                 name="name"
-//                 onChange={handleChanges}
-//                 ref={register({ required: true })} />
+            <input className="form-input"
+                type="number" placeholder="age"
+                name="age"
+                onChange={handleChanges}
+                ref={register({ required: true })} />
 
-//             <input className="form-input"
-//                 type="number" placeholder="age"
-//                 name="age"
-//                 onChange={handleChanges}
-//                 ref={register({ required: true })} />
+            <input className="form-input"
+                type="text" placeholder="height"
+                name="height"
+                onChange={handleChanges}
+                ref={register({ required: true })} />
 
-//             <input className="form-input"
-//                 type="text" placeholder="height"
-//                 name="height"
-//                 onChange={handleChanges}
-//                 ref={register({ required: true })} />
-
-//             <input type="submit" className="submitButton" />
-//         </form>
-//     </div>)
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    )
 }
 
 export default PostSmurfs;
